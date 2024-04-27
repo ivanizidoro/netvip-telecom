8596fa04c6ebfb9c532d1b3e3f43376f7f7ee9e0
Ivan Izidoro <109913632+ivanizidoro@users.noreply.github.com>
2024-04-27 09:21:37 -0300

    remoção-video-plano

--- src/containers/Planos/index.js
+++ src/containers/Planos/index.js
@@ -6,8 +6,6 @@ import plano200 from "../../assets/200.png"
 import plano300 from "../../assets/300.png"
 import plano400 from "../../assets/400.png"
 import plano600 from "../../assets/600.png"
-import Video from "../../assets/Vídeo Comercial NetVip.mp4"
-
 
 function planos() {
 
@@ -36,7 +34,6 @@ function planos() {
           <button>ASSINE JÁ</button></div>
       </ContainerItens>
       <ContainerSobre>
-        <video src={Video} autoPlay loop muted/>
         <h1>QUEM SOMOS</h1>
         <a>Somos… o Grupo Netvip , uma equipe altamente qualificada em atender primordialmente nossos clientes com zelo e total dedicação desde 2009.</a>
       </ContainerSobre>
