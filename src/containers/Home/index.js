93e74ace0da5b70e69d21811f5494d093eebd5a7
ivanizidoro <ivanizidoro@hotmail.com>
2024-05-06 20:12:18 -0300

    adicionado-butap-whast

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { useState } from "react";
 import Nav from "../../components/Nav";
 import Top from "../../components/Top";
@@ -7,9 +8,10 @@ import Local from "../../components/Local";
 import Footer from "../../components/Footer";
 import Slider from "../../components/Slider";
 import MenuMobile from "../../components/MenuMobile";
+import { IoLogoWhatsapp } from "react-icons/io5";
 
 import {
-  Container,
+  Container, ContainerWhats
 } from "./styles"
 
 
@@ -24,6 +26,11 @@ function Home() {
         setMenuIsVisible={setMenuIsVisible}
       />
       <Container>
+        <ContainerWhats>
+          <a href="https://api.whatsapp.com/send/?phone=5581994030800&text&type=phone_number&app_absent=0" target="_blank">
+          <IoLogoWhatsapp />
+          </a>
+        </ContainerWhats>
         <Top />
         <Nav
           setMenuIsVisible={setMenuIsVisible}
@@ -33,7 +40,7 @@ function Home() {
         <Sobre />
         <Local />
         <Footer />
-      </Container>
+      </Container >
     </>
   )
 
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -2,3 +2,34 @@ import styled from "styled-components";
 
 
 export const Container = styled.div``
+
+export const ContainerWhats = styled.div`
+
+    a{
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1000;
+
+    width:80px;
+    height:80px;
+
+    text-align:center;
+    font-size: 60px;
+
+    background-color:#25d366;
+    color: #fff;
+    box-shadow: 1px 1px 2px #888;
+
+    border: none;
+    border-radius: 50%;
+    bottom:30px;
+
+    right:40px;
+
+    cursor: pointer;
+}
+
+`
